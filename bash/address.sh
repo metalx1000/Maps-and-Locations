09314965edec638a4b11c98e2c7fc0bb9a747edd
metalx1000 <github@filmsbykris.com>
2015-01-17 12:39:28 -0500

    First commit

---
+++ bash/address.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#examples: 
+#lat=26.26152844
+#long=-81.51112916
+
+if [ "$#" -lt 2 ]
+then
+  echo "input error"
+  echo "example useage: $0 26.26152844 -81.51112916"
+  exit 1
+fi
+
+lat=$1
+long=$2
+
+wget -O- -q "http://maps.google.com/maps/api/geocode/xml?latlng=$lat,$long&sensor=false"|\
+  grep formatted|\
+  head -n1|\
+  cut -d\> -f2|\
+  cut -d\< -f1
