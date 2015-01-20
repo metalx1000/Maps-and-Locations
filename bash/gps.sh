c04864d500a4e0deb9eb50bf0af06ac8d5d0a3b0
metalx1000 <github@filmsbykris.com>
2015-01-20 08:59:22 -0500

    Added script to get GPS by address

---
+++ bash/gps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]
+then
+  echo "input error"
+  echo "usage: $0 <adderss>"
+  echo "example useage: $0 '123 5th Ave S Naples FL'"
+  exit 1
+fi
+
+address=$1
+
+wget -O- -q "https://maps.googleapis.com/maps/api/geocode/json?address=$address"|\
+  grep -A2 '"location"'|\
+  tail -n2|\
+  cut -d\: -f2|\
+  tr '\n' ' '
+
+echo ""
